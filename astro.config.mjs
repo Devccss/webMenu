062598f65ae42f3479a8006893ddd90dec29c760
Deivid Sandoval Cid <deivid.sandoval.cid@gmail.com>
2026-08-23 13:17:00 -0400

    adapter cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
- import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
- import netlify from '@astrojs/netlify';
+import cloudflare from '@astrojs/cloudflare';
 
- export default defineConfig({
-    // ...
-    adapter: netlify(),
- });
+export default defineConfig({
+   // ...
+   adapter: cloudflare(),
+});
