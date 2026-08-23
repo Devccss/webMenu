33edbaa5bbbf7db46de1acdc6aab7126883e2a39
Deivid Sandoval Cid <deivid.sandoval.cid@gmail.com>
2026-08-22 20:52:57 -0400

    adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+ import { defineConfig } from 'astro/config';
+ import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
-export default defineConfig({
-  // Configure base path automatically for GitHub Pages unless running locally or custom domain is used
-  adapter: netlify({
-    middlewareMode: 'edge',
-  }),
-});
+ export default defineConfig({
+    // ...
+    adapter: netlify(),
+ });
